ca6f13d96c0b83ee8c79969c45db5e1acffa184a
Ben <benklassen19@icloud.com>
2024-04-21 13:58:27 -0600

    scroll

--- docs/index.js
+++ docs/index.js
@@ -3,15 +3,82 @@ const tabContact = document.querySelector("#header-top-tab-contact");
 const tabLocate = document.querySelector("#header-top-tab-locate");
 const tabAbout = document.querySelector("#header-top-tab-about");
 const headerTab = document.querySelector("#header-top-tabs");
+const headerCon2 = document.querySelector("#header-con-2");
 const sectionService = document.querySelector("#section-service");
 const sectionContact = document.querySelector("#section-contact");
 const sectionLocate = document.querySelector("#section-locate");
 const sectionAbout = document.querySelector("#section-about");
+const bodyCon = document.querySelector("#body-con");
+
+let dataPositionTabsCon = headerTab.dataset.position;
+
+const removeClasses = function (which) {
+  const tabWhich = document.querySelector(`#${which}`);
+
+  tabService.classList.remove("header-top-tabs-tabs-active");
+  tabContact.classList.remove("header-top-tabs-tabs-active");
+  tabLocate.classList.remove("header-top-tabs-tabs-active");
+  tabAbout.classList.remove("header-top-tabs-tabs-active");
+  tabService.classList.add("header-top-tabs-tabs");
+  tabContact.classList.add("header-top-tabs-tabs");
+  tabLocate.classList.add("header-top-tabs-tabs");
+  tabAbout.classList.add("header-top-tabs-tabs");
+
+  tabWhich.classList.remove("header-top-tabs-tabs");
+  tabWhich.classList.add("header-top-tabs-tabs-active");
+};
 
 headerTab.addEventListener("click", (e) => {
-  const sectionPosition = document.querySelector(
-    `#${e.target.dataset.scrollto}`
-  );
-  const position = sectionPosition.offsetTop;
-  window.scrollTo(0, position);
+  const tabsHeight = headerTab.offsetHeight;
+  if (e.target.dataset.btn === "btn") {
+    const sectionPosition = document.querySelector(
+      `#${e.target.dataset.scrollto}`
+    );
+    const position = sectionPosition.offsetTop;
+    window.scrollTo(0, position - tabsHeight);
+
+    removeClasses(e.target.id);
+  }
+});
+
+window.addEventListener("scroll", () => {
+  const headerHeight = headerCon2.offsetHeight;
+  const tabsHeight = headerTab.offsetHeight;
+  const position = headerTab.getBoundingClientRect();
+  const positionHeader = headerCon2.getBoundingClientRect();
+  dataPositionTabsCon = headerTab.dataset.position;
+  if (dataPositionTabsCon === "norm") {
+    if (position.top < 1) {
+      headerTab.classList.add("header-top-tabs-fixed");
+      headerTab.classList.remove("header-top-tabs");
+      headerTab.dataset.position = "fixed";
+      bodyCon.style.paddingTop = tabsHeight + 32 + "px";
+    }
+  }
+  if (dataPositionTabsCon === "fixed") {
+    if (positionHeader.top > -headerHeight) {
+      headerTab.classList.add("header-top-tabs");
+      headerTab.classList.remove("header-top-tabs-fixed");
+      headerTab.dataset.position = "norm";
+      bodyCon.style.paddingTop = `32px`;
+    }
+  }
+
+  const positionS = sectionService.getBoundingClientRect().top;
+  const positionC = sectionContact.getBoundingClientRect().top;
+  const positionL = sectionLocate.getBoundingClientRect().top;
+  const positionA = sectionAbout.getBoundingClientRect().top;
+
+  if (positionS + tabsHeight < 0) {
+    removeClasses("header-top-tab-service");
+  }
+  if (positionC + tabsHeight < 0) {
+    removeClasses("header-top-tab-contact");
+  }
+  if (positionL + tabsHeight < 0) {
+    removeClasses("header-top-tab-locate");
+  }
+  if (positionA + tabsHeight < 0) {
+    removeClasses("header-top-tab-about");
+  }
 });
